2e4c0278cb470255aaf2f6b837ac3df5ba7c09c7
mylesk <11828103+myleskeeffe@users.noreply.github.com>
2025-03-13 20:49:45 +0800

    fix: Prevent members from being iterated on group if group has no members. Add better debug logging.

--- scim/handle_group.py
+++ scim/handle_group.py
@@ -3,6 +3,7 @@ from typing import TYPE_CHECKING, Union
 
 from config import IDP_GROUP_TO_ROOM
 from synapse.room import add_to_rooms
+from utils import LogLevel, log
 
 if TYPE_CHECKING:
     from scim.main import SCIMGroup, SCIMGroupUpdate
@@ -11,14 +12,17 @@ if TYPE_CHECKING:
 def process(group: Union['SCIMGroup', 'SCIMGroupUpdate']):
     assigned_rooms = IDP_GROUP_TO_ROOM.get(group.externalId, [])
 
-    for member in group.members:
-        # Check member is of type User
-        if member.ref == "Group":
-            continue
+    if group.members is not None:
+        for member in group.members:
+            # Check member is of type User
+            if member.ref == "Group":
+                continue
 
-        # Add User to Rooms
-        matrix_id = member.value
-        add_to_rooms(matrix_id, assigned_rooms)
+            # Add User to Rooms
+            log(LogLevel.DEBUG, f"Attempting to add {member.value} to rooms: {assigned_rooms}")
+            matrix_id = member.value
+            add_to_rooms(matrix_id, assigned_rooms)
 
-    # TODO: Remove users from rooms if they are no longer in the group
-    # Need to fetch all users in each effected room and compare to group members
+        # TODO: Remove users from rooms if they are no longer in the group
+        # Need to fetch all users in each effected room and compare to group members
+    log(LogLevel.DEBUG, f"Processed group: {group.displayName} ({group.externalId}), but found no members.")
